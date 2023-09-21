acb6520206a68c918a0b2c5bcb20800b377644de
cassius <cassius.avila@edu.pucrs.br>
2023-09-21 11:35:31 -0300

    feat: inclusão de endpoint para buscar uma noticia

--- src/main/java/com/example/painelatualizacaoesacademicas/api/controller/NewsController.java
+++ src/main/java/com/example/painelatualizacaoesacademicas/api/controller/NewsController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,17 +21,31 @@ public class NewsController {
     @GetMapping
     public ResponseEntity<List<News>> get() {
         try {
-            List<News> news = newsService.findAll();
+            List<News> allNews = newsService.findAll();
 
-            if (news.isEmpty()) {
+            if (allNews.isEmpty()) {
                 return ResponseEntity.noContent().build();
             }
 
-            return ResponseEntity.ok(news);
+            return ResponseEntity.ok(allNews);
         }
              catch (Exception e) {
                 return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
             }
-        }
-
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<News> getById(@PathVariable Long id) {
+        try {
+            News news = newsService.find(id);
+
+            if(news == null) {
+                return ResponseEntity.noContent().build();
+            }
+
+            return ResponseEntity.ok(news);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+     }
+}
--- src/main/java/com/example/painelatualizacaoesacademicas/service/impl/NewsServiceImpl.java
+++ src/main/java/com/example/painelatualizacaoesacademicas/service/impl/NewsServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.painelatualizacaoesacademicas.service.impl;
 
 
 import com.example.painelatualizacaoesacademicas.entity.News;
+import com.example.painelatualizacaoesacademicas.exception.AcademicEventNotFoundException;
 import com.example.painelatualizacaoesacademicas.mapper.AcademicEventMapper;
 import com.example.painelatualizacaoesacademicas.mapper.NewsMapper;
 import com.example.painelatualizacaoesacademicas.repository.NewsRepository;
@@ -9,6 +10,7 @@ import com.example.painelatualizacaoesacademicas.service.NewsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,5 +26,17 @@ public class NewsServiceImpl implements NewsService {
         return allAcademicEvent.stream()
                 .map(NewsMapper.MAPPER::mapToNews)
                 .toList();
+    }
+
+    @Override
+    public News find(Long id) {
+        Optional<News> news = newsRepository.findById(id);
+
+        if(news.isPresent()) {
+            return NewsMapper.MAPPER.mapToNews(news.get());
+        } else {
+            throw new AcademicEventNotFoundException("Notícia com ID " + id + " não foi encontrada.");
+        }
+
     }
 }
