f4109cbd1b362158d99f39ef3111c2f7433b433d
vinicio Müller <V.Rodrigues93@edu.pucrs.br>
2023-09-21 18:28:36 -0300

    Criando estrutura para endpoints dos comunicados

--- src/main/java/com/example/painelatualizacaoesacademicas/api/controller/AnnouncementController.java
+++ src/main/java/com/example/painelatualizacaoesacademicas/api/controller/AnnouncementController.java
@@ -1,6 +1,7 @@
 package com.example.painelatualizacaoesacademicas.api.controller;
 
 import com.example.painelatualizacaoesacademicas.entity.Announcement;
+import com.example.painelatualizacaoesacademicas.entity.record.DadosCadastroAcademicEvent;
 import com.example.painelatualizacaoesacademicas.entity.record.DadosCadastroComunicado;
 import com.example.painelatualizacaoesacademicas.service.AnnouncementService;
 import jakarta.validation.Valid;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("api/annoucements")
@@ -22,6 +24,35 @@ private final AnnouncementService announcementService;
     public ResponseEntity<DadosCadastroComunicado> createNewannoucement(@Valid @RequestBody DadosCadastroComunicado comunicado) {
         DadosCadastroComunicado anno = announcementService.createNewannoucement(comunicado);
         return ResponseEntity.status(HttpStatus.CREATED).body(anno);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DadosCadastroComunicado>> findAllAnnouncements() {
+        try {
+            List<DadosCadastroComunicado> comunicados = announcementService.findAllAnnouncements();
+
+            if (comunicados.isEmpty()) {
+                return ResponseEntity.noContent().build();
+            }
+
+            return ResponseEntity.ok(comunicados);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<DadosCadastroComunicado> findById(@PathVariable Long id) {
+        try {
+            DadosCadastroComunicado announcement = announcementService.findAnnouncentById(id);
+
+            if (announcement == null) {
+                return ResponseEntity.notFound().build();
+            }
+
+            return ResponseEntity.ok(announcement);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
     }
 
 }
---
+++ src/main/java/com/example/painelatualizacaoesacademicas/exception/AnnouncementNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.painelatualizacaoesacademicas.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class AnnouncementNotFoundException extends RuntimeException{
+    private String message;
+    public AnnouncementNotFoundException(String message){
+        super(message);
+    }
+}
