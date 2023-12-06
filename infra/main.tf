2056b6aeeb47b71cdd6686d1c00e9a168f2c3048
AleYasSusu <52973893+AleYasSusu@users.noreply.github.com>
2023-12-05 23:53:01 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,16 @@
-#Define chaves e região
-provider "aws" {
-  region     = "us-east-1"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
 }
 
-#Define a instância EC2
+provider "aws" {
+  region = "us-east-1"
+}
+
 resource "aws_instance" "server1" {
   ami           = "ami-0a313d6098716f372"
   instance_type = "t2.micro"
